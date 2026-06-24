254c245ae789d1c230f211367d0b86821c71c45b
DHARSHIYAN <dharshiyan.3112@gmail.com>
2026-06-24 10:45:41 +0530

    Time: 14 ms (93.42%) | Memory: 51.4 MB (73.93%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,24 +1,42 @@
-class Solution {
-    public int findContentChildren(int[] g, int[] s) {
-        int cookiesNums = s.length;
-        if(cookiesNums == 0)  return 0;
+class Solution{
+    public int findContentChildren(int[] g, int[] s){
         Arrays.sort(g);
         Arrays.sort(s);
+        int content=0, cookie_index=0;
+        while(cookie_index < s.length && content < g.length){
+            if(s[cookie_index]>= g[content]){
+                content++;
+            }
+            cookie_index++;
+            
 
-        int maxNum = 0;
-        int cookieIndex = cookiesNums - 1;
-        int childIndex = g.length - 1;
-        while(cookieIndex >= 0 && childIndex >=0){
-            if(s[cookieIndex] >= g[childIndex]){
-                maxNum++;
-                cookieIndex--;
-                childIndex--;
-            }
-            else{
-                childIndex--;
-            }
         }
-
-        return maxNum;
+        return content;
     }
 }
+
+
+// class Solution {
+//     public int findContentChildren(int[] g, int[] s) {
+//         int cookiesNums = s.length;
+//         if(cookiesNums == 0)  return 0;
+//         Arrays.sort(g);
+//         Arrays.sort(s);
+//         int maxNum = 0;
+//         int cookieIndex = cookiesNums - 1;
+//         int childIndex = g.length - 1;
+//         while(cookieIndex >= 0 && childIndex >=0){
+//             if(s[cookieIndex] >= g[childIndex]){
+//                 maxNum++;
+//                 cookieIndex--;
+//                 childIndex--;
+//             }
+//             else{
+//                 childIndex--;
+//             }
+//         }
+
+//         return maxNum;
+//     }
+// }
+
